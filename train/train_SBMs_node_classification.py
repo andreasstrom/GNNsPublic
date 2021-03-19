04f69246cd8de25b3840fa3a38e96303945e1d1d
Andreas Ström <andreascstrom@gmail.com>
2021-03-19 15:23:54 +0100

    test

--- train/train_SBMs_node_classification.py
+++ train/train_SBMs_node_classification.py
@@ -39,7 +39,7 @@ def train_epoch_sparse(model, optimizer, device, data_loader, epoch):
         epoch_train_acc += accuracy(batch_scores, batch_labels)
     epoch_loss /= (iter + 1)
     epoch_train_acc /= (iter + 1)
-    print(model[0].weight.grad)
+    print(model.weight.grad)
     return epoch_loss, epoch_train_acc, optimizer
 
 
